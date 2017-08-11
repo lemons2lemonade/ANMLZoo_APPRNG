65590a8a93e7aadb40ae81b8caca5e4c6d0936f8
Matthew Wallace <matt.wallace.223@gmail.com>
2017-08-11 12:15:51 -0400

    Fixed a number of bounds errors. One or more left.

--- prng_automata.cpp
+++ prng_automata.cpp
@@ -23,34 +23,49 @@ vector<char> createSymbolArray(uint32_t length)
 
 
 vector<vector<char>> alphDivider(int numStates, vector<char> alphArray){
+
+  cout << "Starting alph divider..." << endl;
   
   vector<vector<char>> transitionList;
   int subListEntriesNum = alphArray.size()/(numStates*numStates);
   transitionList.resize(numStates, vector<char>(subListEntriesNum));//Want to create a list of lists and have each sublist be subListEntriesNum large
   int numSq = numStates*numStates; 
-  vector< vector<char> >::iterator row;
-  vector<char>::iterator col;
-  
+
+
+  //FIX THE TYPE ERRORS IN THE LOOPS IMMEDIATELY BELOW
+  // Initialize Vectors
   for(int row = 0; row < numSq; row++) {
-
+    vector<char> column;
+  
     for(int col = 0; col < subListEntriesNum; col++) {
+      //transitionList.at(row).push_back('\0'); //CHECK THIS LINE
+      column.push_back('\0');
+      
+    }
+    transitionList.push_back(column);
+  }
 
+  // Fill vectors with data
+  for(int row = 0; row < numSq; row++) {
+    //cout << "Row: "<<row<<endl;
+    for(int col = 0; col < subListEntriesNum; col++) {
+      //cout << "Col: " <<col<<endl;
       int randIndex = rand() % alphArray.size();
-      cout << randIndex<<endl;
-      cout << alphArray.size()<<endl;
+      // cout << "AlphDivider Index Accessed: " << randIndex << endl;
+      //cout << "AlphDivider Size: " << alphArray.size()<<endl;
       transitionList.at(row).at(col) = alphArray[randIndex];
       alphArray.erase(alphArray.begin()+randIndex);
-     
     }
   }
 
+  cout << "Ending alphDivider..." << endl;
+  
     return transitionList;
 
 }
 
-void create(int numMarkovChains, int numStates,vector<char> alphArr){
+void create(int numMarkovChains, int numStates,vector<char> alphArr,Automata* a){
 
-  Automata* a = new Automata;
 
   for(int i = 0; i < numMarkovChains; i++){
     vector<vector<char>> splitArray = alphDivider(numStates,alphArr);
@@ -69,12 +84,40 @@ void addNewMarkovChain(Automata* a,int numStates,vector<vector<char>> splitArr){
   tOutDynamicVars.resize(numStates*numStates);
   tInDynamicVars.resize(numStates*numStates);
   */
+  cout << "Starting addNewMarkovChain..."<<endl;
   std::unordered_map<string, Element *> elements = a->getElements();
   int countI = 0;
   int countJ = 0;
+  cout <<"Before double for-loop..."<<endl;
   for (int i = 0; i < numStates; i++){
+    cout <<"Starting first for-loop..."<<endl;
         STE *r = new STE("r"+to_string(i),"[*]","all-inputs");
-	STE *s = new STE("s"+to_string(i), "", ""); // Check what string will cause this transition
+	STE *s = new STE("s"+to_string(i), "", ""); //Check what string will cause this transition
+	cout << "Starting first if branch..."<<endl;
+	if(i == 0){
+	//Set stay transition set for each report node
+	  cout << "Inside first if branch..."<<endl;
+	  cout << "SplitArr.size(): "<<splitArr.size()<<endl;
+	  cout <<"splitArr.at(0).size(): "<<splitArr.at(19).size()<<endl;
+	  for (int k = 0; k < splitArr.at(splitArr.size()-1).size()-1; k++){
+	    cout << "K: " << k<<endl;
+	    cout << "I: " <<i<<endl;
+	    cout << "ITEM ADDED TO TRANSITION: " << splitArr.at(countI).at(k) << endl;
+	    s->addSymbolToSymbolSet(splitArr.at(countI).at(k));
+
+	  }
+	}
+        else{
+	     for (int k = 0; k < splitArr.at(countI).size(); k++){
+	       cout << "K: " << k<<endl;
+	       cout << "I: " <<i<<endl;
+	       cout << "ITEM ADDED TO TRANSITION: "<< splitArr.at(i).at(k) << endl;
+	       s->addSymbolToSymbolSet(splitArr.at(countI).at(k));
+	     }
+	}
+       
+  
+	
 	a->rawAddSTE(r);
 	a->rawAddSTE(s);
 	
@@ -84,6 +127,9 @@ void addNewMarkovChain(Automata* a,int numStates,vector<vector<char>> splitArr){
 	
     for (int j = 0; j < numStates; j++){
        if (i != j){
+
+	      countI++;
+	      //countJ++;
 	      string out_path = "t_"+to_string(i)+"-"+to_string(j);
 	      string in_path = "t_"+to_string(j)+"-"+to_string(i);
 	      //Creating outbound transition node and creating a path from the base node 
@@ -92,6 +138,11 @@ void addNewMarkovChain(Automata* a,int numStates,vector<vector<char>> splitArr){
 	      //tOutDynamicVars.at(count) = new STE(out_path,"",""); //SET TRANSITION STRING HERE
 	      //a->rawAddSTE(tOutDynamicVars.at(count));
 	      a->rawAddSTE(new STE(out_path,"",""));
+	      //add transition set for outpath
+	      for (int l = 0; l < splitArr.at(countI).size(); l++){
+		//add chars in here
+		static_cast<STE*>(elements[out_path])->addSymbolToSymbolSet(splitArr.at(countI).at(l));
+	      }
 
 	      
 	      a->addEdge(elements["r"+to_string(i)],elements[out_path]); //mcs[count]
@@ -101,34 +152,56 @@ void addNewMarkovChain(Automata* a,int numStates,vector<vector<char>> splitArr){
 	      // OLD WAY: STE *t_in+path = new STE("t_in"+path,!str+to_string(i+1),""); // Check what string will cause this transition
 	      // tInDynamicVars.at(count) = new STE(out_path,"",""); //SET TRANSITION STRING HERE
 	      // a->rawAddSTE(tInDynamicVars.at(count));
-	      a->rawAddSTE(new STE(in_path,"",""));
+
 	      
+	      a->rawAddSTE(new STE(in_path,"",""));
+	      //add transition set for inpath
+	      if (i == numStates-1 and j== numStates-1){
+		for (int m = 0; m < splitArr.at(0).size(); m++){
+	           //add chars in here
+		  static_cast<STE*>(elements[out_path])->addSymbolToSymbolSet(splitArr.at(countI).at(m)); 
+		}
+	      }
+	      else{
+		for (int m = 0; m < splitArr.at(countI+1).size(); m++){
+		   //add chars in here
+		  static_cast<STE*>(elements[out_path])->addSymbolToSymbolSet(splitArr.at(countI).at(m));
+		}
+	      }
 	      a->addEdge(elements["r"+to_string(j)],elements[in_path]); //mcs[count]
 	      a->addEdge(elements[in_path],elements["r"+to_string(i)]); //mcs[count]
 
-	      countI++;
-	      countJ+=3;
+	    
       
     }
   }
-    	countI++;
+    
   }
 }
 
 
 int main(int argc, char * argv[]) {
   
-  cout << "printing" <<endl;
+  cout << "Starting Main..." <<endl;
+  
+  std::vector<std::vector<char>> test = alphDivider(2,createSymbolArray(256));
 
-  std::vector<std::vector<char>> test = alphDivider(8,createSymbolArray(256));
+  /*
   for(int i = 0; i < test.size(); i++){
     for(int j = 0; j < test.at(i).size(); j++){
+      cout << "Row: "+ to_string(i)+ "Col: "+ to_string(j) <<endl;
       cout << test.at(i).at(j)<<endl;
     }
   }
+  */
 
-  Automata ap;
+  cout << "Running example program..." << endl;
+  
+  Automata* a = new Automata;
 
+  create(2,4,createSymbolArray(256),a);
+  
+  /*
   STE *start = new STE("start", "[Matt]","all-input");
   STE *stop = new STE("stop", "[Bimmy]", "none");
   stop->setReporting(true);
@@ -156,10 +229,11 @@ int main(int argc, char * argv[]) {
   //simulate the automata on two inputs
   ap.simulate('M');
   ap.simulate('J');
-  
+  */
   
   //print out how many reports we've seen (should be 2)
-  cout << ap.getReportVector().size() <<endl;
+  cout << "Report Vector:"<<endl;
+  cout << a->getReportVector().size() <<endl;
   
 
 }
